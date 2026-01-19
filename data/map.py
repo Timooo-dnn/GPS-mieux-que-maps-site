972eb69d1090544703c02befc6cf7a3d74bafdf6
Gwen-83 <gwenael.drouet@gmail.com>
2026-01-19 14:40:42 +0100

    Echantillon données

--- data/map.py
+++ data/map.py
@@ -5,7 +5,7 @@ D = "Départementale"
 
 {"Toulouse":{"Blagnac":[10,A], "Colomiers":[15, N],"Tournefeuille":[8, A]},
 "Blagnac":{"Toulouse":[10, A], "Aussonne":[9,D], "Colomiers":[7, V]},
-"Colomiers":{"Toulouse":[15, N], "Blagnac":[7, V],"Tournefeuille":[5,D], "Aussone":[12,N]},
+"Colomiers":{"Toulouse":[15, N], "Blagnac":[7, V],"Tournefeuille":[5,D], "Aussonne":[12,N]},
 "Tournefeuille":{"Toulouse":[8,A], "Colomiers":[5, D]},
-"Aussone":{"Blagnac":[9,D], "Colomiers":[12, N]}
+"Aussonne":{"Blagnac":[9,D], "Colomiers":[12, N]}
 }
