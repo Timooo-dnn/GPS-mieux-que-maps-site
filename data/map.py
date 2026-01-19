4f9272aa203ecd621e287570eea71c3b1a812a14
Timoo-dnn <tdonini73@gmail.com>
2026-01-19 14:26:56 +0100

    échantillon données

---
+++ data/map.py
@@ -0,0 +1,11 @@
+A = "Autoroute"
+V = "Voie rapide"
+N = "Nationale"
+D = "Départementale"
+
+{"Toulouse":{"Blagnac":[10,A], "Colomiers":[15, N],"Tournefeuille":[8, A]},
+"Blagnac":{"Toulouse":[10, A], "Aussonne":[9,D], "Colomiers":[7, V]},
+"Colomiers":{"Toulouse":[15, N], "Blagnac":[7, V],"Tournefeuille":[5,D], "Aussone":[12,N]},
+"Tournefeuille":{"Toulouse":[8,A], "Colomiers":[5, D]},
+"Aussone":{"Blagnac":[9,D], "Colomiers":[12, N]}
+}
