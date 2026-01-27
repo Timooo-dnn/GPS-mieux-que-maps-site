874f66ed56a71f7f035403212dd754ed00eca3e2
thknight05 <lietkynes10@gmail.com>
2026-01-27 11:32:08 +0100

    localisation with true data

--- src/localisation.py
+++ src/localisation.py
@@ -1,5 +1,11 @@
+import json
 localisation_ville = {"Toulouse":[43.6044638, 1.4442433],
  "Blagnac": [43.6343476, 1.3986403],
  "Colomiers": [43.6112476, 1.3367443],
  "Tournefeuille": [0.5832062, 1.3160025],
  "Aussonne":[43.6343470, 1.3986400]}
+
+with open(r"src/data/dico_final.json",encoding="utf-8") as f:
+    dico_brut = json.load(f)
+
+print(dico_brut["Toulouse_26686518"])
