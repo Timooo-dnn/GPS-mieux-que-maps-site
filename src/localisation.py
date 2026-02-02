b0954cc4bfb3542cf0a611b01c36684f1e21874c
Antoine Hardy <antoine.hardy.149@gmail.com>
2026-02-02 09:11:06 +0100

    Emmerdes N°2190U2408972489072490724

--- src/localisation.py
+++ src/localisation.py
@@ -5,5 +5,5 @@ localisation_ville_test = {"Toulouse":[43.6044638, 1.4442433],
  "Tournefeuille": [0.5832062, 1.3160025],
  "Aussonne":[43.6343470, 1.3986400]}
 
-with open(r"src\data\localisation_villes.json",encoding="utf-8") as f:
+with open(r"src/data/localisation_villes.json",encoding="utf-8") as f:
     localisation_ville = json.load(f)
